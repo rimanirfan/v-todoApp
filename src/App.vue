8e091377d7139152595169f071c8bf510f8341ce
rimanirfan <irfanriman@gmail.com>
2018-11-01 22:33:11 +0700

    implement add and remove todos

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app" class="container">
+    <img class="logo" src="./assets/logo.png">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TodoList,
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  font-size: 24px;
+  margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <input 
+            type="text" 
+            class="todo-input" 
+            placeholder="What needs to be done?" 
+            v-model="newTodo"
+            @keyup.enter="addTodo">
+        <div class="todo-item" v-for="(todo, index) in todos" :key="todo.id">
+            <div>
+                {{ todo.title }}
+            </div>
+            <div class="remove-item" @click="removeTodo(index)">
+                &times;
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodo: '',
+            idTodo: 3,
+            todos: [
+                {
+                    'id': 1,
+                    'title': 'Finish Vue Screentest',
+                    'completed': false,
+                },
+                {
+                    'id': 2,
+                    'title': 'Take over world',
+                    'completed': false,
+                }
+            ],
+        };
+    },
+    methods: {
+        addTodo() {
+            if(this.newTodo.trim().length == 0) {
+                return
+            }
+
+            this.todos.push({
+                id: this.idTodo,
+                title: this.newTodo,
+                completed: false,
+            })
+
+            this.newTodo = ''
+            this.idTodo++
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1)
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .todo-input {
+        width: 100%;
+        padding: 10px 18px;
+        margin-bottom: 16px;
+        font-size: 16px;
+    }
+
+    .todo-item {
+        margin-bottom: 15px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .remove-item {
+        cursor: pointer;
+        margin-left: 15px;
+
+        &:hover {
+            color:black;
+        }
+
+    }
+</style>
+
