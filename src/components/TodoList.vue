cd115ea3a528c3130d1c01049c699c3c79c7c371
rimanirfan <irfanriman@gmail.com>
2018-11-02 21:46:26 +0700

    implement edit todo, cancel edit, done edit, complete todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,12 +7,18 @@
             v-model="newTodo"
             @keyup.enter="addTodo">
         <div class="todo-item" v-for="(todo, index) in todos" :key="todo.id">
-            <div>
-                {{ todo.title }}
+            <div class="todo-item-left">
+                <input type="checkbox" name="" id="" v-model="todo.completed">
+                <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed: todo.completed }">{{ todo.title }}</div>
+                <input v-else class="todo-item-edit" type="text" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus v-model="todo.title">
             </div>
             <div class="remove-item" @click="removeTodo(index)">
                 &times;
             </div>
+        </div>
+        <div class="extra-container">
+            <div><label for=""><input type="checkbox" name="" id="">Check all</label></div>
+            <div>{{ remaining }} items left</div>
         </div>
     </div>
 </template>
@@ -22,20 +28,35 @@ export default {
     data() {
         return {
             newTodo: '',
+            beforeEditTitle: '',
             idTodo: 3,
             todos: [
                 {
                     'id': 1,
                     'title': 'Finish Vue Screentest',
                     'completed': false,
+                    'editing': false,
                 },
                 {
                     'id': 2,
                     'title': 'Take over world',
                     'completed': false,
+                    'editing': false,
                 }
             ],
         };
+    },
+    directives: {
+        focus: {
+            inserted: function (el) {
+                el.focus()
+            }
+        }
+    },
+    computed: {
+        remaining() {
+            return this.todos.filter(todo => !todo.completed).length
+        }
     },
     methods: {
         addTodo() {
@@ -51,6 +72,21 @@ export default {
 
             this.newTodo = ''
             this.idTodo++
+        },
+        editTodo(todo) {
+            this.beforeEditTitle = todo.title
+            todo.editing = true
+        },
+        doneEdit(todo) {
+            if(todo.title.trim().length == 0) {
+                todo.title = this.beforeEditTitle
+            }
+
+            todo.editing = false
+        },
+        cancelEdit(todo) {
+            todo.title = this.beforeEditTitle
+            todo.editing = false;
         },
         removeTodo(index) {
             this.todos.splice(index, 1)
@@ -74,6 +110,31 @@ export default {
         justify-content: space-between;
     }
 
+    .todo-item-left {
+        display: flex;
+        align-items: center;
+    }
+
+    .todo-item-label {
+        padding: 10px;
+        border: 1px solid #fff;
+        margin-left: 12px;
+    }
+
+    .todo-item-edit {
+        font-size: 24px;
+        color: #2c3e50;
+        margin-left: 12px;
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        font-family: 'Avenir', Arial, Helvetica, sans-serif;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
     .remove-item {
         cursor: pointer;
         margin-left: 15px;
@@ -81,7 +142,39 @@ export default {
         &:hover {
             color:black;
         }
+    }
 
+    .completed {
+        color:grey;
+        text-decoration: line-through;
+    }
+
+    .extra-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 16px;
+        border-top: 1px solid lightgrey;
+        padding-top: 14px;
+        margin-bottom: 14px;
+    }
+
+    button {
+        font-size: 14px;
+        background-color: #fff;
+        appearance: none;
+
+        &:hover {
+            background: lightgreen;
+        }
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    .active {
+        background: lightgreen;
     }
 </style>
 
