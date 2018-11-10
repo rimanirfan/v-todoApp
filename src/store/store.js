4d8869b83db348421e645c3357dd1fa3f697b4c4
rimanirfan <irfanriman@gmail.com>
2018-11-10 22:08:13 +0700

    refactor computed properties to store getters

---
+++ src/store/store.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    state: {
+        filter: 'all',
+        todos: [
+            {
+                'id': 1,
+                'title': 'Finish Vue Screentest',
+                'completed': false,
+                'editing': false,
+            },
+            {
+                'id': 2,
+                'title': 'Take over world',
+                'completed': false,
+                'editing': false,
+            }
+        ],
+    },
+    getters: {
+        remaining(state) {
+            return state.todos.filter(todo => !todo.completed).length
+        },
+        anyRemaining(state, getters) {
+            return getters.remaining != 0
+        },
+        todosFiltered(state) {
+            if (state.filter == 'all'){
+                return state.todos
+            } else if (state.filter == 'active') {
+                return state.todos.filter(todo => !todo.completed)
+            } else if (state.filter == 'completed') {
+                return state.todos.filter(todo => todo.completed)
+            }
+        },
+        showClearCompletedBtn(state) {
+            return state.todos.filter(todo => todo.completed).length > 0
+        },
+    }
+})
